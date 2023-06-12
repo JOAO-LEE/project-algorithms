bd0261e2ba9dc3eee0d868617376c74907997c0f
João Lee <joaumlimaum@gmail.com>
2023-06-12 18:28:28 -0300

    Requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-#vqv!
+    invalid_target_time = type(target_time) is not int
+    best_time = 0
+    if target_time == invalid_target_time:
+        return None
+    for period in permanence_period:
+        invalid_permanence_period = type(period[0]) is not int \
+            or type(period[1]) is not int
+        if invalid_permanence_period:
+            return None
+        if period[0] <= target_time <= period[1]:
+            best_time += 1
+    return best_time
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
