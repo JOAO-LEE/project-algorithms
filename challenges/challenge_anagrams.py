629b4bfcde4a6e51bd8cad39c8991a89fbbdcdbb
João Lee <joaumlimaum@gmail.com>
2023-06-13 13:25:18 -0300

    Começando requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,20 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def sort(word):
+
+    for i in range(0, len(word)):
+        tmp = word[i]
+        j = i-1;
+    print(fruits)
+    while (j > -1 and fruits[j] > tmp):
+        fruits[j+1] = fruits[j] 
+        j = j-1
+        fruits[j+1] = tmp
+
+
+sort('edcba')
+
+
+
+
+# def is_anagram(first_string, second_string):
+#     sorted = my_sort([first_string, second_string])
+#     return sorted
