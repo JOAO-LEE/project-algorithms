31e4415af421b67cb24164a88001c723f699d18b
João Lee <joaumlimaum@gmail.com>
2023-06-13 04:02:04 -0300

    Requisito 3 concluído

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
-    raise NotImplementedError
+
+    if len(word) < 2:
+        return True
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
+
+
+print(is_palindrome_recursive("abbsssawa", 0, len("abbsssawa") - 1))
