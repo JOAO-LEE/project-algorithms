6c7317d6f44043f486783505fa8f711eee620328
João Lee <joaumlimaum@gmail.com>
2023-06-13 03:39:41 -0300

    Revert "Requisito 2 concluído"
    This reverts commit d92950a85b5cc1bfbd84e556a86a7701f943f160.

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,19 +1,5 @@
-import pytest
-from challenges.challenge_encrypt_message import encrypt_message
+# from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message(message="abcdef", key="!")
-
-    with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message(message=12312312, key=2)
-
-    encryptedBondinhoEven = encrypt_message("bondinho", 2)
-    assert encryptedBondinhoEven == "ohnidn_ob"
-
-    encryptedBondinhoOdd = encrypt_message("bondinho", 5)
-    assert encryptedBondinhoOdd == "idnob_ohn"
-
-    encryptedBondinhoOdd = encrypt_message("bondinho", 10)
-    assert encryptedBondinhoOdd == "ohnidnob"
+    pass
