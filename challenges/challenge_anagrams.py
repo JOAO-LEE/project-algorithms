ae90f49b80271b2ff343de215cd445e92944a98b
João Lee <joaumlimaum@gmail.com>
2023-06-14 15:22:53 -0300

    Requisito 4 agora concluído - trocado merge sort para quick sort devido alta complexidade do algoritmo anterior

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,53 +1,21 @@
-def merge(left, right):
-
-    if len(left) == 0:
-        return right
-
-    if len(right) == 0:
-        return left
-
-    result = []
-    index_left = index_right = 0
-
-    while len(result) < len(left) + len(right):
-
-        if left[index_left] <= right[index_right]:
-            result.append(left[index_left])
-            index_left += 1
-        else:
-            result.append(right[index_right])
-            index_right += 1
-
-        if index_right == len(right):
-            result += left[index_left:]
-            break
-
-        if index_left == len(left):
-            result += right[index_right:]
-            break
-
-    return result
-
-
-def merge_sort(array):
-
-    if len(array) < 2:
-        return array
-
-    midpoint = len(array) // 2
-    return merge(
-        left=merge_sort(array[:midpoint]), right=merge_sort(array[midpoint:])
-    )
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
 
 
 def is_anagram(first_string, second_string):
-    invalid_value = first_string == '' and second_string == ''
+    invalid_value = first_string == "" and second_string == ""
     if invalid_value:
         return (first_string, second_string, False)
-    first_word = "".join(merge_sort(first_string.lower()))
-    second_word = "".join(merge_sort(second_string.lower()))
+    first_word = "".join(quicksort(first_string.lower()))
+    second_word = "".join(quicksort(second_string.lower()))
 
     return (first_word, second_word, first_word == second_word)
 
 
-print(is_anagram("", ""))
+print(is_anagram("ababc", "babac"))
