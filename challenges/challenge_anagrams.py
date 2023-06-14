6bc89b250addcad85ab184411b6a24d9e0954123
João Lee <joaumlimaum@gmail.com>
2023-06-14 19:44:57 -0300

    Requisito 5 concluído

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,10 +6,9 @@ def quicksort(arr):
         left = [x for x in arr[1:] if x < pivot]
         right = [x for x in arr[1:] if x >= pivot]
         return quicksort(left) + [pivot] + quicksort(right)
-
-
+    
 def is_anagram(first_string, second_string):
-    invalid_value = first_string == "" and second_string == ""
+    invalid_value = first_string == '' and second_string == ''
     if invalid_value:
         return (first_string, second_string, False)
     first_word = "".join(quicksort(first_string.lower()))
@@ -17,5 +16,8 @@ def is_anagram(first_string, second_string):
 
     return (first_word, second_word, first_word == second_word)
 
+
+# Reference of the quick-sort algorithm used above.
+# https://www.geeksforgeeks.org/python-program-for-quicksort/
 
 print(is_anagram("ababc", "babac"))
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,28 @@
+def is_valid_item(item, position):
+    if type(item[position]) != int:
+        return False
+    elif not item[position] >= 0:
+        return False
+    else:
+        return True
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or nums == "" or nums == [] or len(nums) == 1:
+        return False
+
+    sorted_nums = sorted(nums)
+    array_length = len(nums)
+    found_duplicate = False
+
+    for index in range(array_length - 1):
+        is_valid = is_valid_item(sorted_nums, index)
+        if not is_valid:
+            return False
+        if sorted_nums[index] == sorted_nums[index + 1]:
+            found_duplicate = sorted_nums[index]
+
+    return found_duplicate
+
+
+print(find_duplicate([1, 3, 4, 2, 77, 77]))
