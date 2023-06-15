5c7702e189260045845e7f9a817bcabbf66e110d
João Lee <joaumlimaum@gmail.com>
2023-06-15 11:18:02 -0300

    Requisito 6 concluído

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,22 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    word_length = len(word)
+
+    if word_length <= 2:
+        return True
+
+    word_start = 0
+    word_end = word_length - 1
+
+    for index in range(word_length):
+        word_start += 1
+        word_end -= 1
+        if word[word_start] == word[word_end]:
+            return True
+        else:
+            return False
+
+
+print(is_palindrome_iterative("REVIVER"))
